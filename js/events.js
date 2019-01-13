41a942f2b262c260e04674e0a3b721e589724eb9
khensura.loveel Love EL <khensura.loveel@gmail.com>
2019-01-13 09:32:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,17 @@
 //define functions here
 
+
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+
+function frameIt(){
+  $('.tasty').on('load', function(){
+  });
+}
 $(document).ready(function(){
 
 // call functions here
